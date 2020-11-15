d07924fdc150d5e3ddee67d2d152e2eb3f1aa50b
LisethPerez <liseth.perez@uptc.edu.co>
2020-11-15 02:27:42 -0500

    Changes_v19

--- volverStock.php
+++ volverStock.php
@@ -15,34 +15,35 @@ foreach ($datos as $product) {
     $cantidad_pro = $product['cantidad'];
 
    // print_r($product);
+   include 'conexionGene.php';
+    $consulta = "SELECT * FROM producto WHERE nombre='{$nombre_producto}'";
+    $sql = mysqli_query($conn,$consulta) or die(mysqli_error($conn));
+
+    if($numFilas = $sql->num_rows>0){
+        $result = $sql->fetch_object();
+        $id = $result->id_producto;
+    }
+    include 'conexionBD.php';
+    $consult = "SELECT * FROM stock WHERE producto_id_producto='{$id}' AND sede_id_sede='{$id_Sede}'";
+    $sql1 = mysqli_query($conn,$consult) or die(mysqli_error($conn));
+    if($numFilas1 = $sql1->num_rows>0){
+        $result1 = $sql1->fetch_object();
+        $cantidad = $result1->disponibilidad;
+        $cantidad = $cantidad + $cantidad_pro;
+
+        //echo $cantidad;
+    }
+
+    $consult1 = "UPDATE stock SET disponibilidad={$cantidad} WHERE producto_id_producto='{$id}' AND sede_id_sede='{$id_Sede}'";
+    $sql2 = mysqli_query($conn,$consult1) or die(mysqli_error($conn));
+
+    if($sql2){
+        $mensaje = "Adicción realizada";
+    }else{
+        $mensaje =  "No se realizaron cambios";
+    }
 }
 
-include 'conexionGene.php';
-$consulta = "SELECT * FROM producto WHERE nombre='{$nombre_producto}'";
-$sql = mysqli_query($conn,$consulta) or die(mysqli_error($conn));
-
-if($numFilas = $sql->num_rows>0){
-    $result = $sql->fetch_object();
-    $id = $result->id_producto;
-}
-include 'conexionBD.php';
-$consult = "SELECT * FROM stock WHERE producto_id_producto='{$id}' AND sede_id_sede='{$id_Sede}'";
-$sql1 = mysqli_query($conn,$consult) or die(mysqli_error($conn));
-if($numFilas1 = $sql1->num_rows>0){
-    $result1 = $sql1->fetch_object();
-    $cantidad = $result1->disponibilidad;
-    $cantidad = $cantidad + $cantidad_pro;
-
-    //echo $cantidad;
-}
-
-$consult1 = "UPDATE stock SET disponibilidad={$cantidad} WHERE producto_id_producto='{$id}' AND sede_id_sede='{$id_Sede}'";
-$sql2 = mysqli_query($conn,$consult1) or die(mysqli_error($conn));
-
-if($sql2){
-    echo "Adicción realizada";
-}else{
-    echo "No se realizaron cambios";
-}
+echo $mensaje;
 
 ?>
