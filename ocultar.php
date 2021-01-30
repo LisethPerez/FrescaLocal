8de3908a1970c084d094c59e009f021ae1b85146
LisethPerez <liseth.perez@uptc.edu.co>
2021-01-30 01:00:45 -0500

    Changes_v51

--- ocultar.php
+++ ocultar.php
@@ -2,9 +2,8 @@
 require 'conexionGene.php';
 
 $produ = $_GET['var'];
-
 $consult = "SELECT * FROM producto WHERE nombre='{$produ}'";
-    
+
 $sql = mysqli_query($conn,$consult) or die(mysqli_error($conn));
 
 if($numFilas = $sql->num_rows>0){
@@ -12,9 +11,10 @@ if($numFilas = $sql->num_rows>0){
     $result = $sql->fetch_object();
     $unidad = $result->unidad_de_medida;
     //$image = imagecreatefromjpeg($result->imagen);
+
     if($unidad=="KILO"){
         echo "peso";
-    }if($unidad=="UNIDAD"){
+    } else{
         echo "cantidad";
     }
 
