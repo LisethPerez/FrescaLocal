087f0e59e816c1d9760d118fbbb97c911173a133
LisethPerez <julianaaaa131@gmail.com>
2021-05-31 11:33:28 -0500

    Changes_v66

--- cerrar_caja.php
+++ cerrar_caja.php
@@ -7,7 +7,8 @@ $base = $_POST['var'];
 $ingresosEfe = $_POST['var2'];
 $ingresosElec = $_POST['var3'];
 $egresos = $_POST['var4'];
-$fecha1 = date('Y/m/d H:i:s');
+$web = $_POST['var5'];
+$fecha1 = date('Y-m-d H:i:s');
 $fecha = date("Y-m-d") . " 00:00:00";
 $diaSiguiente = date("Y-m-d", strtotime("+1 day", strtotime($fecha))) . " 00:00:00";
 
@@ -23,37 +24,77 @@ $sqlUser = mysqli_query($conn,$consultEmple) or die(mysqli_error($conn));
 $resultadoUser = $sqlUser->fetch_object();
 $idEmple = $resultadoUser->id_empleado;
 
-$consultCaja = "INSERT INTO caja(base_monetaria,ingresos_efectivo,ingresos_electronicos,egresos_efectivo,egresos_electronicos,ventas,fecha,pagos,cierre,empleado_id_empleado,sede_id_sede) VALUES ('$base','$ingresosEfe','$ingresosElec','$egresos',0,'$total','$fecha1',0,0,'$idEmple','$id_Sede')";
-$sqlCaja = mysqli_query($conn,$consultCaja) or die(mysqli_error($conn));
-//$resultadoCaja = $sqlCaja->fetch_object();
+$nuevaFecha = substr($fecha1, 0, 10);
 
-if($sqlCaja){
+$caja = "SELECT * FROM caja WHERE fecha LIKE '%{$nuevaFecha}%'";
+$sqlCaj = mysqli_query($conn,$caja) or die(mysqli_error($conn));
+
+if($numFilas = $sqlCaj->num_rows>0){
+    $resultCaja = $sqlCaj->fetch_object();
+    $idCaja = $resultCaja->id_caja;
+
+    $cajaModi = "UPDATE caja SET base_monetaria='{$base}', ingresos_efectivo='{$ingresosEfe}', ingresos_electronicos='{$ingresosElec}', egresos_efectivo='{$egresos}', egresos_electronicos='{$web}', ventas='{$total}', fecha='{$fecha1}' WHERE id_caja='{$idCaja}'";
+    $sqlMoodi = mysqli_query($conn,$cajaModi) or die(mysqli_error($conn));
+    if($sqlMoodi){
     
-    $username="control3_cosechafresca2";
-    $password="vk{j@%zq2HWq";
-
-    try {
-        $mbd = new PDO('mysql:host=controler.com.co;dbname=control3_prueba2',$username,$password, array(PDO::ERRMODE_WARNING));
-        $mbd->query($consultCaja);
-    } catch (PDOException $e) {
-        //echo 'Falló la conexión: ' . $e->getMessage();
-        
-        //$sql1 = mysqli_query($conn1,$consulta1) or die(mysqli_error());
-        
-        $file = fopen("sincronizacion/sentenciasBD.txt","a+");
-        //$file = fopen('sentencias.txt', 'w');
-        fwrite($file, '<?php'. PHP_EOL);
-        fwrite($file, '$conn = mysqli_connect("controler.com.co","control3_cosechafresca2","vk{j@%zq2HWq","control3_prueba2") or die(mysqli_error());'. PHP_EOL);
-        fwrite($file, '$consulta1="'.$consultCaja.'";' . PHP_EOL);
-        fwrite($file, '$sql1 = mysqli_query($conn,$consulta1) or die(mysqli_error());' . PHP_EOL);
-        fwrite($file, '?>'. PHP_EOL);
-        fclose($file); 
+        $username="control3_cosechafresca2";
+        $password="vk{j@%zq2HWq";
+    
+        try {
+            $mbd = new PDO('mysql:host=controler.com.co;dbname=control3_prueba2',$username,$password, array(PDO::ERRMODE_WARNING));
+            $mbd->query($cajaModi);
+        } catch (PDOException $e) {
+            //echo 'Falló la conexión: ' . $e->getMessage();
+            
+            //$sql1 = mysqli_query($conn1,$consulta1) or die(mysqli_error());
+            
+            $file = fopen("sincronizacion/sentenciasBD.txt","a+");
+            //$file = fopen('sentencias.txt', 'w');
+            fwrite($file, '<?php'. PHP_EOL);
+            fwrite($file, '$conn = mysqli_connect("controler.com.co","control3_cosechafresca2","vk{j@%zq2HWq","control3_prueba2") or die(mysqli_error());'. PHP_EOL);
+            fwrite($file, '$consulta1="'.$cajaModi.'";' . PHP_EOL);
+            fwrite($file, '$sql1 = mysqli_query($conn,$consulta1) or die(mysqli_error());' . PHP_EOL);
+            fwrite($file, '?>'. PHP_EOL);
+            fclose($file); 
+        }
+    
+        $mensaje = "Caja correcta";
+    }else{
+        $mensaje = "Ha ocurrido un error";
     }
-
-    echo "Caja correcta";
 }else{
-    echo "Ha ocurrido un error";
+    //echo "No hay registro, se va a crear una nuevo";
+    $consultCaja = "INSERT INTO caja(base_monetaria,ingresos_efectivo,ingresos_electronicos,egresos_efectivo,egresos_electronicos,ventas,fecha,pagos,cierre,empleado_id_empleado,sede_id_sede) VALUES ('$base','$ingresosEfe','$ingresosElec','$egresos','$web','$total','$fecha1',0,0,'$idEmple','$id_Sede')";
+    $sqlCaja = mysqli_query($conn,$consultCaja) or die(mysqli_error($conn));
+
+    if($sqlCaja){
+    
+        $username="control3_cosechafresca2";
+        $password="vk{j@%zq2HWq";
+    
+        try {
+            $mbd = new PDO('mysql:host=controler.com.co;dbname=control3_prueba2',$username,$password, array(PDO::ERRMODE_WARNING));
+            $mbd->query($consultCaja);
+        } catch (PDOException $e) {
+            //echo 'Falló la conexión: ' . $e->getMessage();
+            
+            //$sql1 = mysqli_query($conn1,$consulta1) or die(mysqli_error());
+            
+            $file = fopen("sincronizacion/sentenciasBD.txt","a+");
+            //$file = fopen('sentencias.txt', 'w');
+            fwrite($file, '<?php'. PHP_EOL);
+            fwrite($file, '$conn = mysqli_connect("controler.com.co","control3_cosechafresca2","vk{j@%zq2HWq","control3_prueba2") or die(mysqli_error());'. PHP_EOL);
+            fwrite($file, '$consulta1="'.$consultCaja.'";' . PHP_EOL);
+            fwrite($file, '$sql1 = mysqli_query($conn,$consulta1) or die(mysqli_error());' . PHP_EOL);
+            fwrite($file, '?>'. PHP_EOL);
+            fclose($file); 
+        }
+    
+        $mensaje = "Caja correcta";
+    }else{
+        $mensaje = "Ha ocurrido un error";
+    }
 }
-//$idEmple = $resultadoUser->id_empleado;
+echo $mensaje;
 
 ?>
